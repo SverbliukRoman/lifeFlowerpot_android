4e50009aa448258a7ce021f57b53916c8f8b5659
SverbliukRoman <amor201075@hotmail.com>
2019-06-28 20:41:54 +0300

    Create user manager for firebase

---
+++ app/src/main/java/com/sverbusoft/lifeflowerpot/managers/UserManager.kt
@@ -0,0 +1,9 @@
+package com.sverbusoft.lifeflowerpot.managers
+
+import com.google.firebase.auth.FirebaseAuth
+
+class UserManager {
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+
+}
