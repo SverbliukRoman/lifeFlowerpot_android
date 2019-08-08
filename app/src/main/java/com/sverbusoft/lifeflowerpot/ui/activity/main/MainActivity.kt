13237f1ac42174d03a0853faf0c71fa29243474e
SverbliukRoman <amor201075@hotmail.com>
2019-08-08 18:08:24 +0300

    Fixed crash after login

--- app/src/main/java/com/sverbusoft/lifeflowerpot/ui/activity/main/MainActivity.kt
+++ app/src/main/java/com/sverbusoft/lifeflowerpot/ui/activity/main/MainActivity.kt
@@ -8,19 +8,19 @@ import com.sverbusoft.lifeflowerpot.R
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textMessage: TextView
+    //private lateinit var textMessage: TextView
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                textMessage.setText(R.string.title_home)
+                //textMessage.setText(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                textMessage.setText(R.string.title_dashboard)
+                //textMessage.setText(R.string.title_dashboard)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_profile -> {
-                textMessage.setText(R.string.title_profile)
+                //textMessage.setText(R.string.title_profile)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -32,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        textMessage = findViewById(R.id.message)
+        //textMessage = findViewById(R.id.message)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
     }
 }
